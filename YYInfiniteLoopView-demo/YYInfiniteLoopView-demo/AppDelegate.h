180f4695573ecb08127db96dd70b2ce2ab5ce131
Arvin <yasir86@126.com>
2016-09-02 11:13:54 +0800

    initialization

---
+++ YYInfiniteLoopView-demo/YYInfiniteLoopView-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYInfiniteLoopView-demo
+//
+//  Created by Arvin on 16/9/2.
+//  Copyright © 2016年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYInfiniteLoopView-demo/YYInfiniteLoopView-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYInfiniteLoopView-demo
+//
+//  Created by Arvin on 16/9/2.
+//  Copyright © 2016年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
